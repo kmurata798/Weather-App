e71cd7561abe2a04889e45c5462496f7468c1ebb
kmurata798 <kmurata798@gmail.com>
2020-06-08 02:34:25 -0700

    Attempting mood posting.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 The ```.flaskenv``` file allows for a faster startup when running the server
+
+1. Added additional templates for jinja usage
+2. created view_city route
+3. change city names from 'span' to 'a href='
+4. hooked up view_city route to the 'a href=' tag for the city name so that users can view the specific city
+
+Need to:
+1. allow users to post a message/mood into the view_city page.
--- app.py
+++ app.py
@@ -16,6 +16,10 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+class Mood(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mood = db.Column(db.String(30), nullable=False)
+
 def get_weather_data(city):
     # OpenWeatherMap API url
     url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=imperial&appid=a95bbf99e1761eac5543637aac409d6e'
@@ -49,17 +53,21 @@ def index_get():
             'description': r['weather'][0]['description'],
             'icon': r['weather'][0]['icon'],
         }
+        # post = {
+        #     'mood': 
+        # }
 
         # Append the requested data into the weather_data list
         weather_data.append(weather)
         # Display requested data in terminal to test out server
         print(weather)
 
-    return render_template('weather.html', weather_data=weather_data)
+    return render_template('index.html', weather_data=weather_data)
 
 @app.route('/', methods=['POST'])
 def index_post():
     err_msg = ''
+    # Get 'city' field from user input
     new_city = request.form.get('city')
 
     # Check if user input a city
@@ -108,4 +116,16 @@ def delete_city(name):
     # Send user back to main index page.
     return redirect(url_for('index_get'))
     # NOTE: Even though you delete a city from the database, you still have access to it until you exit the route
-    
+
+@app.route('/view/<name>', methods=['POST'])
+def view_city(name):
+# View city
+    # Get 'mood' field from user input
+    new_mood = request.form.get('mood')
+    # Store the entered city into this variable as an object
+    new_mood_obj = Mood(name=new_mood)
+
+    # Add city to database as an object
+    db.session.add(new_mood_obj)
+    # Confirm changes
+    db.session.commit()
