37d8cd600a882ea7cb3b7e88a93241b3e3bc7516
kmurata798 <kmurata798@gmail.com>
2020-06-08 01:21:30 -0700

    [Add] Display FLASH messages on success/error.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
@@ -7,6 +7,8 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 # SQLALCHEMY_TRACK_MODIFICATIONS gets rid of the SQLAlchemy message that displays everytime I run the server
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# SECRET_KEY allows the use of FLASH MESSAGES
+app.config['SECRET_KEY'] = 'thisisasecret'
 
 db = SQLAlchemy(app)
 
@@ -14,13 +16,18 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+def get_weather_data(city):
+    # OpenWeatherMap API url
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=imperial&appid=a95bbf99e1761eac5543637aac409d6e'
+    r = requests.get(url).json()
+    return r
 
 @app.route('/')
 def index_get():
     # Variable that holds all the cities in my sqlite3 database table -> is a query from the City table
     cities = City.query.all()
     # OpenWeatherMap API url
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=a95bbf99e1761eac5543637aac409d6e'
+    # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=a95bbf99e1761eac5543637aac409d6e'
     # # Temporary test data
     # city = 'Los Angeles'
 
@@ -31,8 +38,10 @@ def index_get():
 
         # Requesting info from weatherAPI. 'url.format(city)' inserts my 'city' variable into the {} inside url variable.
         # r => all data included in the API
-        r = requests.get(url.format(city.name)).json()
-        print(r)
+        # city.name comes from the for loop above, 'city'
+        # r = requests.get(url.format(city.name)).json()
+        # print(r)
+        r = get_weather_data(city.name)
 
         weather = {
             'city': city.name,
@@ -55,22 +64,31 @@ def index_post():
 
     # Check if user input a city
     if new_city:
-        # Declare a variable to grab/hold the city IF it already exists in database == City already exists
-        existing_city = City.query.filter_by(name=new_city)
+        # Declare a variable to grab/hold the first city in API IF it already exists in database == City already exists
+        existing_city = City.query.filter_by(name=new_city).first()
 
         # If the city does not already exist...
         if not existing_city:
-            new_city_obj = City(name=new_city)
+            # Check if entered city is actually a city and store result into this variable
+            new_city_data = get_weather_data(new_city)
+            if new_city_data['cod'] == 200:
+                # Store the entered city into this variable as an object
+                new_city_obj = City(name=new_city)
 
-            # Add city to database as an object
-            db.session.add(new_city_obj)
-            # Confirm changes
-            db.session.commit()
-
+                # Add city to database as an object
+                db.session.add(new_city_obj)
+                # Confirm changes
+                db.session.commit()
+            else:
+                err_msg = 'City does not exist!!!'
         # If the city already exists...
         else:
             # Send error message instead...
-            err_msg = 'City already exists!!!'
+            err_msg = 'City already exists in database!!!'
 
+    if err_msg:
+        flash(err_msg, 'error')
+    else:
+        flash('City Added Successfully!')
     # redirect takes user back to the desired route
     return redirect(url_for('index_get'))
