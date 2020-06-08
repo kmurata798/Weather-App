1a2b74fd986ad87195d8f0907457b68f71b12382
kmurata798 <kmurata798@gmail.com>
2020-06-08 00:31:48 -0700

    [Add] Prevent duplicate cities from being stored.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
@@ -15,15 +15,8 @@ class City(db.Model):
     name = db.Column(db.String(50), nullable=False)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        new_city = request.form.get('city')
-
-        if new_city:
-            new_city_obj = City(name=new_city)
-            db.session.add(new_city_obj)
-            db.session.commit()
+@app.route('/')
+def index_get():
     # Variable that holds all the cities in my sqlite3 database table -> is a query from the City table
     cities = City.query.all()
     # OpenWeatherMap API url
@@ -48,11 +41,36 @@ def index():
             'icon': r['weather'][0]['icon'],
         }
 
+        # Append the requested data into the weather_data list
         weather_data.append(weather)
+        # Display requested data in terminal to test out server
         print(weather)
 
     return render_template('weather.html', weather_data=weather_data)
 
+@app.route('/', methods=['POST'])
+def index_post():
+    err_msg = ''
+    new_city = request.form.get('city')
 
+    # Check if user input a city
+    if new_city:
+        # Declare a variable to grab/hold the city IF it already exists in database == City already exists
+        existing_city = City.query.filter_by(name=new_city)
 
+        # If the city does not already exist...
+        if not existing_city:
+            new_city_obj = City(name=new_city)
 
+            # Add city to database as an object
+            db.session.add(new_city_obj)
+            # Confirm changes
+            db.session.commit()
+
+        # If the city already exists...
+        else:
+            # Send error message instead...
+            err_msg = 'City already exists!!!'
+
+    # redirect takes user back to the desired route
+    return redirect(url_for('index_get'))
